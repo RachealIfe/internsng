19cee19180ddd45f25ac207c2f931473f80f8c18
Akinbobola Akinola <bobolatech@gmail.com>
2019-08-02 17:52:14 +0100

    removed files

--- resources/views/user/company/createquiz.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <p class="lead">Lessons</p>
-
-    <div class="list-group">
-        lessionss
-    </div>
-@endsection
--- resources/views/user/company/meeting.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="pl-5 pr-5">
-    </div>
-
-    <script src="https://source.zoom.us/zoom-meeting-1.5.0.min.js"></script>
-
-@endsection
